dc6e0c312a3f53aba527c51d106cbd9e0e80a986
Joshua Humphries <jh@fullstory.com>
2018-12-11 14:08:52 -0500

    add CSRF protection to grpcui command-line tool and standalone.Handler (#8)

--- standalone/standalone.go
+++ standalone/standalone.go
@@ -2,6 +2,7 @@ package standalone
 
 import (
 	"bytes"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"mime"
@@ -15,6 +16,9 @@ import (
 	"github.com/fullstorydev/grpcui"
 	"github.com/fullstorydev/grpcui/internal/resources/standalone"
 )
+
+const csrfCookieName = "_grpcui_csrf_token"
+const csrfHeaderName = "x-grpcui-csrf-token"
 
 // Handler returns an HTTP handler that provides a fully-functional gRPC web
 // UI, including the main index (with the HTML form), all needed CSS and JS
@@ -30,7 +34,6 @@ import (
 // The returned handler expects to serve resources from "/". If it will instead
 // be handling a sub-path (e.g. handling "/rpc-ui/") then use http.StripPrefix.
 func Handler(ch grpcdynamic.Channel, target string, methods []*desc.MethodDescriptor, files []*desc.FileDescriptor) http.Handler {
-	// TODO(jh): add CSRF protection
 	webFormHTML := grpcui.WebFormContents("invoke", "metadata", methods)
 	webFormJS := grpcui.WebFormScript()
 	webFormCSS := grpcui.WebFormSampleCSS()
@@ -78,12 +81,37 @@ func Handler(ch grpcdynamic.Channel, target string, methods []*desc.MethodDescri
 	})
 
 	rpcInvokeHandler := http.StripPrefix("/invoke", grpcui.RPCInvokeHandler(ch, methods))
-	mux.Handle("/invoke/", rpcInvokeHandler)
+	mux.HandleFunc("/invoke/", func(w http.ResponseWriter, r *http.Request) {
+		// CSRF protection
+		c, _ := r.Cookie(csrfCookieName)
+		h := r.Header.Get(csrfHeaderName)
+		if c == nil || c.Value == "" || c.Value != h {
+			http.Error(w, "incorrect CSRF token", http.StatusUnauthorized)
+			return
+		}
+		rpcInvokeHandler.ServeHTTP(w, r)
+	})
 
 	rpcMetadataHandler := grpcui.RPCMetadataHandler(methods, files)
 	mux.Handle("/metadata", rpcMetadataHandler)
 
-	return &mux
+	// make sure we always have a csrf token cookie
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := r.Cookie(csrfCookieName); err != nil {
+			tokenBytes := make([]byte, 32)
+			if _, err := rand.Read(tokenBytes); err != nil {
+				http.Error(w, "failed to create CSRF token", http.StatusInternalServerError)
+				return
+			}
+			c := &http.Cookie{
+				Name:  csrfCookieName,
+				Value: base64.RawURLEncoding.EncodeToString(tokenBytes),
+			}
+			http.SetCookie(w, c)
+		}
+
+		mux.ServeHTTP(w, r)
+	})
 }
 
 var indexTemplate = template.Must(template.New("index.html").Parse(string(standalone.GetIndexTemplate())))
